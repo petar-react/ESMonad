a3ccd7389b9542cbb5555c4a401ebe0e8c9af8ca
petar-react <petar.m.djurkovic@gmail.com>
2018-02-14 23:38:18 +0100

    Basic event sourcing example

---
+++ src/main/scala/org/esmonad/model.scala
@@ -0,0 +1,21 @@
+package org.esmonad
+
+sealed trait Rotation
+case object ToLeft extends Rotation
+case object ToRight extends Rotation
+
+sealed trait Direction {
+  def rotate(rot: Rotation): Direction =
+    (this, rot) match {
+      case (North, ToLeft) => West; case (North, ToRight) => Est
+      case (South, ToLeft) => Est; case (South, ToRight) => West
+      case (Est, ToLeft) => North; case (Est, ToRight) => South
+      case (West, ToLeft) => South; case (West, ToRight) => North
+    }
+}
+
+case object North extends Direction
+case object South extends Direction
+case object Est extends Direction
+case object West extends Direction
+
